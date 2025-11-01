11bdbd37356fbd5ec40e5074f722fbce35df52b3
Henry LETELLIER <henry.letellier@epitech.eu>
2025-11-01 07:40:20 +0100

    [UPDATE] the __init__.py file to expose the classes of the program

--- rotary_logger/__init__.py
+++ rotary_logger/__init__.py
@@ -22,7 +22,7 @@
 # PROJECT: rotary_logger
 # FILE: __init__.py
 # CREATION DATE: 29-10-2025
-# LAST Modified: 23:31:11 29-10-2025
+# LAST Modified: 7:2:21 01-11-2025
 # DESCRIPTION: 
 # A module that provides a universal python light on iops way of logging to files your program execution.
 # /STOP
@@ -32,11 +32,16 @@
 # +==== END rotary_logger =================+
 """
 
-
+from rotary_logger.entrypoint import Tee
 from rotary_logger.rotary_logger import RotaryLogger
 from rotary_logger.tee_stream import TeeStream
+from rotary_logger.file_instance import FileInstance
+from rotary_logger import constants as CONST
 
 __all__ = [
+    "Tee",
     "RotaryLogger",
-    "TeeStream"
+    "TeeStream",
+    "FileInstance",
+    "CONST"
 ]
